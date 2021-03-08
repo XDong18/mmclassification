fb08d18eb237527f0e923233b60a0546790b4131
XDong18 <liuxd1215@gmail.com>
2021-03-08 20:52:55 +0800

    update

--- mmcls/datasets/bdd100k.py
+++ mmcls/datasets/bdd100k.py
@@ -107,7 +107,7 @@ class BddCls(BaseDataset):
         ]
         eval_results = {}
         results_0 = np.vstack(results[0])
-        print('\npin1\n', results_0.shape, '\npin1\n')
+        # print('\npin1\n', results_0.shape, '\npin1\n')
         results_1 = np.vstack(results[1])
         results_2 = np.vstack(results[2])
         list_results = [results_0, results_1, results_2]
@@ -115,7 +115,7 @@ class BddCls(BaseDataset):
         list_gt_labels = [gt_labels_0, gt_labels_1, gt_labels_2]
 
         num_imgs = len(results[0])
-        print('\npin2\n', len(gt_labels_0), num_imgs, '\npin2\n')
+        # print('\npin2\n', len(gt_labels_0), num_imgs, '\npin2\n')
         # assert len(gt_labels_0) == num_imgs, 'dataset testing results should '\
         #     'be of the same length as gt_labels.'
 
@@ -124,7 +124,7 @@ class BddCls(BaseDataset):
             raise ValueError(f'metirc {invalid_metrics} is not supported.')
 
         topk = metric_options.get('topk', (1,))
-        print('\npin\n', topk, '\npin\n')
+        # print('\npin\n', topk, '\npin\n')
         thrs = metric_options.get('thrs')
         average_mode = metric_options.get('average_mode', 'macro')
 
