aba542977579c38a38cd3aedab42d7f10cc511ed
XDong18 <liuxd1215@gmail.com>
2021-03-08 17:02:58 +0800

    update

--- configs/_base_/models/resnet101_bdd.py
+++ configs/_base_/models/resnet101_bdd.py
@@ -1,7 +1,7 @@
 # model settings
 model = dict(
     type='ImageClassifier',
-    pretrained='open-mmlab://detectron/resnet101_caffe',
+    pretrained='torchvision://resnet101_caffe',
     backbone=dict(
         type='ResNet',
         depth=101,
