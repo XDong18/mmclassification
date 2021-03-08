ca592df8f4ff3a3618b85e0d7a701adc38c12d13
XDong18 <liuxd1215@gmail.com>
2021-03-08 16:26:06 +0800

    update

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,2 +1,2 @@
 export CUDA_VISIBLE_DEVICES=0,1,2,3
-PORT=29502 ./tools/dist_train.sh bdd_mtl/configs/mtl/bdd_dla34up_s.py 4 --work-dir out/resnet101
+PORT=29502 ./tools/dist_train.sh configs/resnet/resnet101_b8x4_bdd100k.py 4 --work-dir out/resnet101_bs32
