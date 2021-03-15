a6ea83438ce3249551ede87310fb289b720917a5
XDong18 <liuxd1215@gmail.com>
2021-03-15 21:15:55 +0800

    update

--- configs/dla/dla34_bdd100k.py
+++ configs/dla/dla34_bdd100k.py
@@ -1,4 +1,4 @@
 _base_ = [
-    '../_base_/models/resnet101_bdd.py', '../_base_/models/dla34_bdd100k.py',
+    '../_base_/datasets/bdd100k.py', '../_base_/models/dla34_bdd100k.py',
     '../_base_/schedules/bdd100k.py', '../_base_/default_runtime.py'
 ]
