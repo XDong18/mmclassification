2f5bed228b0dbf4c34f4a176a2dc0287f3ec2bb4
XDong18 <liuxd1215@gmail.com>
2021-03-16 00:02:38 +0800

    update

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,3 +1,3 @@
-export CUDA_VISIBLE_DEVICES=0,4,5,9
+export CUDA_VISIBLE_DEVICES=0,1,2,3
 PORT=29502 ./tools/dist_train.sh configs/dla/dla34_bdd100k.py 4 --work-dir out/dla34_bs48 \
 --resume-from out/dla34_bs48/latest.pth
